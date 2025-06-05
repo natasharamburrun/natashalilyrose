5aac8bab7fb480d289ae3a248d1fd28a474d67cf
Natasha Ramburrun <r_natasha100@hotmail.com>
2025-06-05 13:52:04 +0100

    05/06 - Added brand logos and colours

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './styles/styles.scss';
+import logo from '../src/assets/images/branding/logo-small-final-packaging.svg'
 
 function App() {
   return (
-    <div className="App">
+        <div className="App">
       <header className="App-header">
-        <h1>NATASHA LILY ROSE JEWELLERY</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          NATASHA LILY ROSE JEWELLERY
+        </h1>
       </header>
     </div>
   );
