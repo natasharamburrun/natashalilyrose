e29c3fb17c7c01ed36cbe48f591c781d0ec868de
Natasha Ramburrun <r_natasha100@hotmail.com>
2025-06-07 13:42:15 +0100

    Added basic product page content cards and styled

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App () {
       <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/' element={<ProductPage />} />
+        <Route path='/Shop' element={<ProductPage />} />
       </Routes>
     </ReactRouter>
   );
