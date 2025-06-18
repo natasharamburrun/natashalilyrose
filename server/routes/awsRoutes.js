6316c4acfcaef6cf25d9f6af7e14dbce5b7d3eef
Natasha Ramburrun <r_natasha100@hotmail.com>
2025-06-18 13:47:43 +0100

    Added the functionality for aws

---
+++ server/routes/awsRoutes.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+
+const express = require("express")
+// const connectDB = require("./config/db");
+const ObjectId = require("mongodb").ObjectId
+require("dotenv").config({path: "./config.env"})
+
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3")
+
+let awsRoutes = express.Router()
+// const s3Bucket = "natashalilyrosestorage"
+
+const s3Client = new S3Client({
+    region: "eu-west-2",
+    Credential: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY
+    }
+
+})
+
+exports.getObject = async(key) => {
+    try {
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET, 
+            Key: key,
+        }
+
+        const command = new GetObjectCommand(params);
+        const data = await s3Client.send(command)
+
+        console.log(data)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+module.exports = awsRoutes
